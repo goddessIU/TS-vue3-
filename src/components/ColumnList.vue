0e22c7196d373d92a14b14470bb50cbfcdd2fc8d
goddessIU <2581606394@qq.com>
2022-02-17 09:51:27 +0800

    first

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul>
+        <li v-for="column in list" :key="column.id">
+            <img :src="column.avatar" :alt="column.title">
+            <h5>{{column.title}}</h5>
+            <p>{{column.description}}</p>
+            <a href="#">进入专栏</a>
+        </li>
+    </ul>
+</template>
+
+<script lang="ts" setup>
+import type { PropType } from 'vue';
+
+export interface ColumnProps {
+    id: number,
+    title: string,
+    avatar: string,
+    description: string
+}
+const props = defineProps({
+    list: {
+        type: Array as PropType<ColumnProps[]>,
+        required: true
+    }
+})
+</script>
